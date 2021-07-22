5fef38424b129e38cd8efbf12c002b47e9fad650
suigang <244153653@qq.com>
2021-07-22 17:31:20 +0800

    手提提交

---
+++ vue相关/vue-组件传参/src/components/propsAndEmit/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+      <div>{{parentObj.title}}</div>
+      <button @click="tellParent">儿子回答父亲</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  props:{
+      parentObj:{
+          type:Object,
+          default:{}
+      }
+  },
+  methods:{
+      tellParent(){
+          this.$emit("tellFater","我在耍泥巴")
+      }
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
---
+++ vue相关/vue-组件传参/src/views/Home.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="home">
+        <div class="box-all">
+            <div class="title">第一种：props和$emit</div>
+            <son :parentObj="parentObj" @tellFater="fromSon" />
+            <div>儿子的回答：{{ sonMessage }}</div>
+        </div>
+        <div class="box-all">
+            <div class="title">第二种：$parent和 $children</div>
+        </div>
+		<div class="box-all">
+            <div class="title">第三种：$attrs和 $listeners</div>
+        </div>
+		<div class="box-all">
+            <div class="title">第四种：$parent和 $children</div>
+        </div>
+		<div class="box-all">
+            <div class="title">第五种：Provide 和 Inject</div>
+        </div>
+		<div class="box-all">
+            <div class="title">第六种：EventBus</div>
+        </div>
+		<div class="box-all">
+            <div class="title">第七种：Vuex</div>
+        </div>
+		<div class="box-all">
+            <div class="title">第八种：observable</div>
+        </div>
+    </div>
+</template>
+
+<script>
+import son from "../components/propsAndEmit/index.vue";
+export default {
+    name: "Home",
+    data() {
+        return {
+            parentObj: {
+                title: "来自父组件的问题:你在干啥？"
+            },
+            sonMessage: ""
+        };
+    },
+    components: {
+        son: son
+    },
+    methods: {
+        fromSon(str) {
+            this.sonMessage = str;
+        }
+    }
+};
+</script>
+<style lang="less">
+.box-all {
+    border: 1px solid #ccc;
+    width: 70%;
+    min-height: 100px;
+    margin: 0 auto;
+    overflow: hidden;
+    text-align: left;
+    padding: 20px;
+    margin-bottom: 10px;
+    .title {
+        font-weight: 600;
+        margin-bottom: 20px;
+    }
+}
+</style>
